ca672ca170386c0aff50c09e25f3d94fe7ba51bf
jgarzon <jgarzon@easysol.net>
2017-07-14 00:16:15 -0500

    env1

---
+++ env-config/env1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ANDROID_HOME=/home/jagarzone/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/build-tools
