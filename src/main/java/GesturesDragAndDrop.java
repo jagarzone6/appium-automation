99b3dc7df86245ec656c84c126d2315622cf6c0e
jgarzon <jgarzon@easysol.net>
2017-07-16 20:43:41 -0500

    Drag and Drop

---
+++ src/main/java/GesturesDragAndDrop.java
@@ -0,0 +1,38 @@
+import driver.appDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.junit.Test;
+
+
+/**
+ * Created by jgarzon on 13/07/17.
+ */
+public class GesturesDragAndDrop {
+
+    @Test
+    public static void main(String[] args) {
+
+        appDriver app= new appDriver();
+        AndroidDriver<AndroidElement> driver = app.capabilities();
+
+        try {
+            AndroidElement Views = driver.findElementByXPath("//android.widget.TextView[@text='Views']");
+            Views.click();
+            driver.findElementByXPath("//android.widget.TextView[@text='Drag and Drop']").click();
+
+            TouchAction t=new TouchAction(driver);
+            t.longPress(driver.findElementById("io.appium.android.apis:id/drag_dot_1")).moveTo(driver.findElementById("io.appium.android.apis:id/drag_dot_3")).release().perform();
+
+
+            driver.quit();
+
+        }catch (Exception e) {
+            System.out.println("\n\rERROR:   ");
+            System.out.println(e);
+            driver.quit();
+        }
+    }
+
+
+}
