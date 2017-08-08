b02ed24a77e4eb793bb267811aafa75032711f02
jgarzon <jgarzon@easysol.net>
2017-08-07 21:40:36 -0500

    hybrid app

--- src/main/java/Android/hybridApp/InitDemo.java
+++ src/main/java/Android/hybridApp/InitDemo.java
@@ -3,6 +3,9 @@ package Android.hybridApp;
 import driver.appDriverAndroidDevice;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+
+import java.util.Set;
 
 /**
  * Created by jagarzone on 7/08/17.
@@ -10,12 +13,32 @@ import io.appium.java_client.android.AndroidElement;
 public class InitDemo {
     public static void main(String[] args) {
 
-        appDriverAndroidDevice app= new appDriverAndroidDevice("testApp.apk");
+        //appDriverAndroidDevice app= new appDriverAndroidDevice("testApp.apk");
+        appDriverAndroidDevice app= new appDriverAndroidDevice("ApiDemos-debug.apk");
         AndroidDriver<AndroidElement> driver = app.capabilities();
 
         try{
+            System.out.println("APP Context : "+driver.getContext());
+            //driver.findElementById("com.example.testapp:id/urlField").sendKeys("http://google.com");
+            //driver.findElementById("com.example.testapp:id/goButton").click();
 
+            AndroidElement Views = driver.findElementByXPath("//android.widget.TextView[@text='Views']");
+            Views.click();
 
+            AndroidElement webView = driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))");
+            webView.click();
+
+            driver.findElement(By.id("android:id/action_bar")).click();
+            //SWITCH VIEW
+            Set<String> setViews = driver.getContextHandles();
+            System.out.println("APP views : "+setViews);
+
+            // "Enable view attribute inspection" in "Developer Options" on Android device.
+            driver.context("WEBVIEW_io.appium.android.apis");
+
+            driver.findElement(By.partialLinkText("Hello World! - 1")).click();
+
+            driver.context("NATIVE_APP");
 
         driver.quit();
         }
